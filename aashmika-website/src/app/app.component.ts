564ff137ddae31f4a6347ce5f1a42dac5bc3b664
unknown <aashmikam@gmail.com>
2018-08-22 18:18:59 -0400

    Add the videos and profile pic

--- aashmika-website/src/app/app.component.ts
+++ aashmika-website/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Aashmikas Personal Website';
+  title = 'Aashmika Mali';
 }
